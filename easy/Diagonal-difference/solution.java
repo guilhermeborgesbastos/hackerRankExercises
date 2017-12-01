70cda40b4578048dcbd4703f7e4150fb235d91fa
Guilherme <guilhermeborgesbastos@gmail.com>
2017-12-01 13:33:17 -0200

    Fixed the main function

--- easy/Diagonal-difference/solution.java
+++ easy/Diagonal-difference/solution.java
@@ -1,69 +1,45 @@
 /*
-Given a square matrix of size , calculate the absolute difference between the sums of its diagonals.
-
-Input Format
-
-The first line contains a single integer, . The next  lines denote the matrix's rows, with each line containing space-separated integers describing the columns.
-
-Constraints
-
-Output Format
-
-Print the absolute difference between the two sums of the matrix's diagonals as a single integer.
-
-Sample Input
-
-3
-11 2 4
-4 5 6
-10 8 -12
-Sample Output
-
-15
-Explanation
-
-The primary diagonal is:
-
-11
-   5
-     -12
-Sum across the primary diagonal: 11 + 5 - 12 = 4
-
-The secondary diagonal is:
-
-     4
-   5
-10
-Sum across the secondary diagonal: 4 + 5 + 10 = 19 
-Difference: |4 - 19| = 15
-
-Note: |x| is absolute value function
+ *
+ * Hacker Rank Execise
+ * @version    0.1.0
+ * @author     Guilherme Borges Bastos
+ * @copyright  Copyright 2017 Guilherme Borges Bastos
+ * @LinkedIn   https://www.linkedin.com/in/guilhermeborgesbastos/
+ * @Facebook   https://www.facebook.com/guilherme.borgesbastos/
+ * @E-mail     guilhermeborgesbastos@gmail.com
+ * 
 */
-
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
 
 public class Solution {
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int input = in.nextInt();
-        for(int i=input; i!=0; i--) { 
-            int spacesNum = i - 1;
-            int hashNum = input - spacesNum;
-            // loop spaces
-            for(int y=spacesNum; y>0; y--){
-              System.out.print(" ");
-            }
-            // loop hash
-            for(int z=hashNum; z>0; z--){
-              System.out.print("#");
-            }
-            //break line
-            System.out.println("");
+  public static void main(String[] args) throws IOException {
+    
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    
+    //Get size of matrix
+    final int N = Integer.parseInt(br.readLine());
+      
+    //Get matrix
+    final int[][] matrix = new int[N][N];
+    for(int y = 0; y < N; ++y){
+        String[] row = br.readLine().split(" ");
+        for(int x = 0; x < N; ++x){
+            matrix[y][x] = Integer.parseInt(row[x]);
         }
     }
+    
+    //Initialize difference
+    long diff = 0;
+    
+    //Get difference of sum of diagonals
+    for(int y = 0, x = N; y < N; ++y){
+        diff += matrix[y][y] - matrix[y][--x];
+    }
+    
+    //Convert to absolute difference
+    diff = (diff < 0) ? -diff : diff;
+      
+    //Print absolute difference
+    System.out.print(diff);
+  }
 }
